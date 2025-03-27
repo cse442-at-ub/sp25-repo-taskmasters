fdff0d629e867ab95b10e29c8bf144db66b97463
leondanquah <leondanq@buffalo.edu>
2025-03-27 16:57:18 -0400

    feat: Implement MonthView calendar with task management and API integration

--- taskmasters/src/App.js
+++ taskmasters/src/App.js
@@ -4,6 +4,7 @@ import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
 import DayView from './components/DayView';
 import WeekView from './components/WeekView';
+import MonthView from './components/MonthView';
 import './index.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/dashboard" element={<DayView />} />
         <Route path="/calendar" element={<DayView />} />
         <Route path="/week-view" element={<WeekView />} />
+        <Route path="/month-view" element={<MonthView />} />
         <Route path="/avatar" element={<div>Avatar Customization Coming Soon</div>} />
         <Route path="/achievements" element={<div>Achievements Coming Soon</div>} />
         <Route path="/create-task" element={<div>Create Task Coming Soon</div>} />
