f7a930c0f24aaa4bc3edc9c376298ad24bea2b7d
JEANKOKI <jeankoki@outlook.com>
2025-04-14 19:54:25 -0400

    Sprint 4: bugs fixed db connection

--- taskmasters/src/config.js
+++ taskmasters/src/config.js
@@ -1,9 +1,9 @@
 const config = {
     // Production API URL
-    // apiUrl: 'https://se-prod.cse.buffalo.edu/CSE442/2025-Spring/cse-442h/tm-backend/api'
+     apiUrl: 'https://se-prod.cse.buffalo.edu/CSE442/2025-Spring/cse-442h/tm-backend/api'
     
     // Local API URL
-    apiUrl: 'http://localhost/tm-backend/api',
+    //apiUrl: 'http://localhost/tm-backend/api',
 
     // Development API URL 
     // apiUrl: 'https://se-dev.cse.buffalo.edu/CSE442/2025-Spring/cse-442h/tm-backend/api'
